3b89b2e956fddecc415f81e5ca23e8b7dd07d1c7
Kevin <keviinnalvarez2015@gmail.com>
2020-02-04 16:56:17 -0400

    crearción de formulario de registro

--- front-read/src/pages/registro/index.js
+++ front-read/src/pages/registro/index.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
+import {
+  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBInput, MDBBtn
+} from 'mdbreact';
 
 export default class Register extends Component {
   render () {
     return (
-      <div>registro</div>
+      <MDBCol>
+        <MDBCard style={{height:'450px'}}>
+          <MDBCardBody>
+            <MDBCardTitle className='text-center mt-3'>Resgistro</MDBCardTitle>
+            <div className='grey-text mr-5 ml-5 mt-5'>
+              <form>
+                <MDBRow center>
+                    <MDBCol lg='6'>
+                      <MDBInput
+                        label="Your name"
+                        icon="user"
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Last name"
+                        icon="user"
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                    </MDBCol>
+
+                    <MDBCol lg='6'>
+                      <MDBInput
+                        label="Your email"
+                        icon="envelope"
+                        group
+                        type="email"
+                        validate
+                        error="wrong"
+                        success="right"
+                      />
+                      <MDBInput
+                        label="Your password"
+                        icon="lock"
+                        group
+                        type="password"
+                        validate
+                      />
+                    </MDBCol>
+                </MDBRow>
+                <div className='text-center mt-4'>
+                  <MDBBtn>Enviar</MDBBtn>
+                </div>
+              </form>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
     );
   }
 }
