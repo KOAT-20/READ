f6165b2d6be5c2d2a800e08705b9ca014f536695
Kevin <keviinnalvarez2015@gmail.com>
2020-02-12 21:57:08 -0400

    nueva dependencia, y correccion para telefono

--- front-read/src/components/mobile/navigation.js
+++ front-read/src/components/mobile/navigation.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { MDBNavbar, MDBNavbarNav, MDBNavItem, MDBIcon } from 'mdbreact';
 import { makeStyles } from '@material-ui/core/styles';
-import { } from '@material-ui/core';
+import {
+  List, ListItem, ListItemText, ListItemIcon, SwipeableDrawer, Divider 
+} from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles({
   list: {
@@ -18,18 +22,62 @@ export default function NavigationMobile (props) {
     left: false,
   });
 
+  const toggleDrawer = (side, open) => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <MDBNavbar color='indigo' style={{boxShadow:'none'}} dark>
-      <MDBNavbarNav left>
-        <MDBNavItem>
-          <MDBIcon icon='bars' className='white-text' size='2x' />
-        </MDBNavItem>
-      </MDBNavbarNav>
-      <MDBNavbarNav right>
-        <MDBNavItem onClick={props.onModal}>
-          <MDBIcon icon='user' className='white-text' size='2x' />
-        </MDBNavItem>
-      </MDBNavbarNav>
-    </MDBNavbar>
+    <div>
+      <MDBNavbar color='indigo' style={{boxShadow:'none'}} dark>
+        <MDBNavbarNav left>
+          <MDBNavItem onClick={toggleDrawer('left', true)}>
+            <MDBIcon icon='bars' className='white-text' size='2x' />
+          </MDBNavItem>
+        </MDBNavbarNav>
+        <MDBNavbarNav right>
+          <MDBNavItem onClick={props.onModal}>
+            <MDBIcon icon='user' className='white-text' size='2x' />
+          </MDBNavItem>
+        </MDBNavbarNav>
+      </MDBNavbar>
+      <SwipeableDrawer
+        open={state.left}
+        onClose={toggleDrawer('left', false)}
+        onOpen={toggleDrawer('left', true)}
+      >
+        {sideList('left')}
+      </SwipeableDrawer>
+    </div>
+
   );
 }
