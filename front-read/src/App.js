e8bcece91e46cdc4ff7febd7a10fc99d1282dfd9
Kevin <keviinnalvarez2015@gmail.com>
2020-06-17 10:44:57 -0400

    refactor

--- front-read/src/App.js
+++ front-read/src/App.js
@@ -1,80 +1,9 @@
-import React, { Component } from 'react';
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { MDBContainer } from 'mdbreact';
-import axios from 'axios';
-/* Components */
-import BarNavigation from './components/navigation.js';
-import Login from './components/login.js';
-/* Mobile */
-import NavigationMobile from './components/mobile/navigation.js';
-/* Pages */
-import Admin from './pages/admin';
+import React from 'react';
 
-class App extends Component {
-  state = {
-    modal14: false,
-    users: [],
-  }
-
-  async componentDidMount () {
-    this.getUsers();
-  }
-
-  getUsers = async () => {
-    const res = await axios.get('http://localhost:3000/api/users');
-    this.setState ({
-      users: res.data
-    })
-    // console.log('Usuarios:', res.data);
-  }
-
-  toggle = nr => () => {
-    let modalNumber = 'modal' + nr
-    this.setState({
-      [modalNumber]: !this.state[modalNumber]
-    });
-  }
-
-  render() {
-    if (isWidthUp('sm', this.props.width)) {
-      return (
-        <Router>
-          <BarNavigation modal={this.state.modal14} onModal={this.toggle(14)} />
-          <Login modal={this.state.modal14} onModal={this.toggle(14)} />
-          <MDBContainer className="mt-5 mb-5">
-            <Route exact path='/'>
-            <div>
-              {this.state.users.map(user => {
-                return (
-                  <div key={user._id}>{user.lastname}</div>
-                )
-              })}
-            </div>
-            </Route>
-            <Route path='/administrador' component={Admin} />
-          </MDBContainer>
-        </Router>
-      );
-    }
-    return (
-      <Router>
-        <NavigationMobile modal={this.state.modal14} onModal={this.toggle(14)} />
-        <Login modal={this.state.modal14} onModal={this.toggle(14)} />
-        <MDBContainer className="mb-5">
-          <Route exact path='/'>
-          <div>
-            {this.state.users.map(user => {
-              return (
-                <div key={user._id}>{user.lastname}</div>
-              )
-            })}
-          </div>
-          </Route>
-          <Route path='/administrador' component={Admin} />
-        </MDBContainer>
-      </Router>
-    );
-  }
+export default function App () {
+  return (
+    <div>
+      hola
+    </div>
+  );
 }
-export default withWidth()(App);
