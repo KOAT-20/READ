98e8895b64244234f038eec6bedf9988c3f89e83
Kevin <keviinnalvarez2015@gmail.com>
2020-06-21 18:18:49 -0400

    get one requets, controller

--- back-read/src/controllers/requests.controllers.js
+++ back-read/src/controllers/requests.controllers.js
@@ -5,3 +5,8 @@ requestsControl.getRequests = async (req, res) => {
   const requests = await requestsModel.find();
   res.json(requests);
 }
+
+requestsControl.getRequest = async (req, res) => {
+  const request = await requestsModel.findById(req.params.id);
+  res.json (request);
+}
