d92b774d06762f0ee8f66c4e52aa70a6f7638dfa
Kevin <keviinnalvarez2015@gmail.com>
2020-06-21 19:15:22 -0400

    changes

--- back-read/src/routes/requests.routes.js
+++ back-read/src/routes/requests.routes.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-// const {
-//   getRequests, getRequest, createRequest, deleteRequest
-// } = require('../controllers/requests.controllers.js');
+const {
+  getRequests, getRequest, createRequest, deleteRequest
+} = require('../controllers/requests.controllers.js');
 
 router.route('/')
-  // .get(getRequests)
-  // .post(createRequest)
+  .get(getRequests)
+  .post(createRequest)
 
 router.route('/:id')
-  // .get(getRequest)
-  // .delete(deleteRequest)
+  .get(getRequest)
+  .delete(deleteRequest)
 
 module.exports = router;
