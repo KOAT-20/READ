0b2efc0555e0a085782eac910d5a77190168f560
Kevin <keviinnalvarez2015@gmail.com>
2020-06-21 18:27:16 -0400

    file controller requests, advanced 50%

--- back-read/src/controllers/requests.controllers.js
+++ back-read/src/controllers/requests.controllers.js
@@ -12,3 +12,33 @@ requestsControl.getRequest = async (req, res) => {
   const request = await requestsModel.findById(req.params.id);
   res.json (request);
 }
+
+requestsControl.createRequest = async (req, res) => {
+  const { name_slc, last_slc, dni_slc, age_slc, phone_slc, email_slc } = req.body;
+  const newRequest = new requestsModel({
+    firstname,
+    lastname,
+    email,
+    password,
+  })
+  await newRequest.save();
+  res.json({message: 'Solicitud creada exitosamente!'})
+}
+
+// requestsControl.updateRequest = async (req, res) => {
+//   const { /* patametros */ } = req.body;
+//   await requestsModel.findOneAndUpdate({_id: req.params.id}, {
+//     /* patametros */
+//   });
+//   res.json({message: 'Solicitud actualizada exitosamente!'})
+// }
+
+requestsControl.deleteRequest = async (req, res) => {
+  const { _id } = req.body;
+  await requestsModel.findOneAndDelete({_id: req.params.id}, {
+    _id,
+  });
+  res.json({message: 'Solicitud eliminada!'})
+}
+
+module.exports = userControl;
