b4d4b9d3834af2c174fd839d6e15428bbb6d18f8
Kevin <keviinnalvarez2015@gmail.com>
2020-06-21 19:05:08 -0400

    Finish controller for create request XD

--- back-read/src/controllers/requests.controllers.js
+++ back-read/src/controllers/requests.controllers.js
@@ -1,7 +1,7 @@
 const requestsControl = {};
 const requestsModel = require('../models/applicant.models.js');
-// const comunityModel = require('../models/comunity.models.js');
-// const reasonModel = require('../models/reason.models.js');
+const comunityModel = require('../models/comunity.models.js');
+const reasonModel = require('../models/reason.models.js');
 
 requestsControl.getRequests = async (req, res) => {
   const requests = await requestsModel.find();
@@ -9,20 +9,37 @@ requestsControl.getRequests = async (req, res) => {
 }
 
 requestsControl.getRequest = async (req, res) => {
-  const request = await requestsModel.findById(req.params.id);
-  res.json (request);
+  const request = await requestsModel.findById({}, (error, req.params.id) => {
+    comunityModel.populate(req.params.id, {path:'Comunity'}, (error, req.params.id) => {
+      reasonModel.populate(req.params.id, {path:'Reason'}, (error, req.params.id) => {
+        res.json (request);
+      });
+    });
+  });
 }
 
 requestsControl.createRequest = async (req, res) => {
-  const { name_slc, last_slc, dni_slc, age_slc, phone_slc, email_slc } = req.body;
+  const {
+    name_slc, last_slc, dni_slc, age_slc, phone_slc, email_slc,
+    name_comunity, state_comunity, mncp_comunity, parish_comunity, postal_c_comunity,
+    type_slctd, amount_slctd, description_slctd
+  } = req.body;
+  /* Applicant */
   const newRequest = new requestsModel({
-    firstname,
-    lastname,
-    email,
-    password,
+     name_slc, last_slc, dni_slc, age_slc, phone_slc, email_slc
   })
   await newRequest.save();
-  res.json({message: 'Solicitud creada exitosamente!'})
+  /* Comunity */
+  const newComunity = new comunityModel({
+     name_comunity, state_comunity, mncp_comunity, parish_comunity, postal_c_comunity
+  })
+  await newComunity.save();
+  /* Reason */
+  const newComunity = new comunityModel({
+     type_slctd, amount_slctd, description_slctd
+  })
+  await newComunity.save();
+  res.json({message: 'Solicitud creada exitosamente!'});
 }
 
 // requestsControl.updateRequest = async (req, res) => {
