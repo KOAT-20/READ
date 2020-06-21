77d4954ada2cdc6896ac090ac30223ef9f6a6fb1
Kevin <keviinnalvarez2015@gmail.com>
2020-06-21 18:20:46 -0400

    require model, for create new requests

--- back-read/src/controllers/requests.controllers.js
+++ back-read/src/controllers/requests.controllers.js
@@ -1,5 +1,7 @@
 const requestsControl = {};
 const requestsModel = require('../models/applicant.models.js');
+// const comunityModel = require('../models/comunity.models.js');
+// const reasonModel = require('../models/reason.models.js');
 
 requestsControl.getRequests = async (req, res) => {
   const requests = await requestsModel.find();
